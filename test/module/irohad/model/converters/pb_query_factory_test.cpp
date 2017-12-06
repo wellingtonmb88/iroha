77b38f3ff853e4704659d834d02e5a4b4c39345e
motxx <motohiko.ave@gmail.com>
2017-12-06 18:53:29 +0900

    Add Pager proto serialize test
    Signed-off-by: motxx <motohiko.ave@gmail.com>

--- test/module/irohad/model/converters/pb_query_factory_test.cpp
+++ test/module/irohad/model/converters/pb_query_factory_test.cpp
@@ -16,7 +16,9 @@
  */
 
 #include <gtest/gtest.h>
+#include <queries.pb.h>
 #include "crypto/hash.hpp"
+#include "model/converters/pb_common.hpp"
 #include "model/converters/pb_query_factory.hpp"
 #include "model/generators/query_generator.hpp"
 
@@ -102,6 +104,19 @@ TEST(PbQueryFactoryTest, get_role_permissions){
 TEST(PbQueryFactoryTest, get_asset_info){
   auto query = QueryGenerator{}.generateGetAssetInfo();
   runQueryTest(query);
+}
+
+/**
+ * @given Model Pager
+ * @when serialize to proto and deserialize it.
+ * @then Validate pager is equal to original type
+ */
+TEST(PbQueryFactoryTest, SerializePager) {
+  decltype(std::declval<Pager>().tx_hash) tx_hash;
+  tx_hash.fill(1);
+  const auto pager = Pager{iroha::hash256_t{}, 1};
+  const auto des_pager = deserializePager(serializePager(pager));
+  ASSERT_EQ(pager, des_pager);
 }
 
 /**
